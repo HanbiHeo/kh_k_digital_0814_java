475653d649eb25cbe5c98a14facb8918d70ca06c
HanbiHeo <hhb0365@gmail.com>
2023-08-29 14:59:24 +0900

    2023.08.29 다중인터페이스상속

---
+++ src/다중인터페이스상속/Aircon.java
@@ -0,0 +1,9 @@
+package 다중인터페이스상속;
+
+public interface Aircon {//인터페이스 만들기
+    int MAX_TEMP = 30; // 상수라서 대문자. 객체화가 안됨. public final static
+    int MIN_TEMP = 0;
+    void airconON();
+    void airconOFF();
+    void setAirconTeap(int tmp);
+}
