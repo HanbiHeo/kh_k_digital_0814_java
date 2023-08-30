b5544b5c361b1616b442a143ff8dc0924b177ec7
HanbiHeo <hhb0365@gmail.com>
2023-08-30 19:01:57 +0900

    2023.08.30 실수 수정

--- src/오브젝트클래스/ObjectClass.java
+++ src/오브젝트클래스/ObjectClass.java
@@ -12,14 +12,14 @@ public class ObjectClass {
 //        Integer test2 = 2300;
 //        System.out.println(test.toString()); //toString() 메소드는 오버라이딩해서 사용 가능하며, String, Integer의 경우에는 이미 오버라이딩 되어 있음.
 //        System.out.println(test); // test는 참조변수. 오버라이딩해서 실제 값을 찾아와줌.
-//        System.out.println(test2); // test는 참조변수. 오버라이딩해서 실제 값을 찾아와줌.
+//        System.out.println(test2);
 //        System.out.println(test2.toString());
 //        int test3 = 4500;
 //        System.out.println(test3);
 
         Car car02 = car01;
         //equals() 메소드 : 인스턴스를 매개변수로 전달받는 참조변수와 비교하여 그 결과를 반환
-        System.out.println(car01.equals(02)); // 사실은 해쉬코드 값을 묻는 내용임. 해쉬코드값이 같다는건 메모리가 같다는 말
+        System.out.println(car01.equals(car02)); // 사실은 해쉬코드 값을 묻는 내용임. 해쉬코드값이 같다는건 메모리가 같다는 말
     }
 }
 
